2925a80035ed41486efe527581a3c8f154cb05c1
Adam Ruzicka <a.ruzicka@outlook.com>
2022-03-03 10:33:57 +0100

    Fix remaining cops and errors

--- test/support/test_execution_log.rb
+++ test/support/test_execution_log.rb
@@ -24,11 +24,13 @@ class TestExecutionLog
   end
 
   def self.setup
-    @run, @finalize = self.new, self.new
+    @run      = self.new
+    @finalize = self.new
   end
 
   def self.teardown
-    @run, @finalize = nil, nil
+    @run      = nil
+    @finalize = nil
   end
 
   def self.run
